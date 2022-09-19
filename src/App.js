caac72d5081d9957cb5fc9895a2ed169fe9cf6b8
Farzin Jabbary <farzin.jabbary@gmail.com>
2022-09-18 18:54:13 -0700

    component created and react router setup done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+import Cart from './components/Cart';
+import Navbar from './components/Navbar';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <div className="App">
-      App
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
